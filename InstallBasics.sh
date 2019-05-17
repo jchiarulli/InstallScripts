8bc9e53aab3696fd621e8c81fcf486ccaef6eadf
jason <jason@Champ.jason>
2019-05-17 13:31:23 -0400

    Updated

---
+++ InstallBasics.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sudo apt install -y curl
+sudo apt install -y cmake
+sudo apt install -y emacs
+sudo apt install -y vim 
+sudo apt install -y compton
+sudo apt install -y i3
+sudo apt install -y i3blocks
+sudo apt install -y htop
+sudo apt install -y glances
+sudo apt install -y calcurse
+sudo apt install -y sl
+sudo apt install -y cowsay
+sudo apt install -y cmatrix
+sudo apt install -y espeak
+sudo apt install -y tmux
+sudo apt install -y git
+sudo apt install -y nitrogen 
+sudo apt install -y feh
+sudo apt install -y scrot
+sudo apt install -y redshift
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
+sudo apt install -y openvpn
+sudo apt install -y python3-pip 
+sudo apt install -y neofetch
+sudo apt install -y screenfetch
+sudo apt install -y ranger
+sudo apt install -y unclutter
+sudo apt install -y dunst
+sudo apt install -y bluetooth
+sudo apt install -y bluez
+sudo apt install -y bluez-tools
+sudo apt install -y blueman
+sudo apt install -y rfkill
+sudo apt install -y terminator
+sudo apt install -y apt-transport-https
+sudo apt install -y openssh-server
+# for irony server in emacs
+sudo apt install -y libclang-dev
+# for some reason I can't pip3 install this in my virtualenv
+sudo apt install -y python3-tk
+sudo apt install -y virtualenv
+sudo apt update 
+sudo apt upgrade -y
